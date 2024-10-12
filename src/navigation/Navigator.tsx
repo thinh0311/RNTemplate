7c94f51675bae471c5e9ed38767da767e3505a24
thinh0311 <thinhxt2000@gmail.com>
2024-10-12 22:48:42 +0700

    safeareaview

--- src/navigation/Navigator.tsx
+++ src/navigation/Navigator.tsx
@@ -5,6 +5,7 @@ import HomeScreen from 'src/screens/HomeScreen';
 import DetailScreen from 'src/screens/DetailScreen';
 import {createDrawerNavigator} from '@react-navigation/drawer';
 import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs';
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
 
 const BottomTab = createBottomTabNavigator<RootStackParamList>();
 
@@ -31,8 +32,9 @@ export const DrawerNavigator: FC = () => {
 const Tab = createMaterialTopTabNavigator<RootStackParamList>();
 
 export const TopTab: FC = () => {
+  const insets = useSafeAreaInsets();
   return (
-    <Tab.Navigator>
+    <Tab.Navigator style={{paddingTop: insets.top}}>
       <Tab.Screen name={KeyScreen.Home} component={HomeScreen} />
       <Tab.Screen name={KeyScreen.Details} component={DetailScreen} />
     </Tab.Navigator>
